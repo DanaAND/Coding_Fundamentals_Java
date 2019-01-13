2cb618429e066e98e249f146d06c814371cf0f26
Dana Andro <dana_marin2003@yahoo.com>
2019-01-13 17:30:02 +0200

    Return middle of array

---
+++ src/main/java/coding/StringReturnMiddle.java
@@ -0,0 +1,30 @@
+package coding;
+
+public class StringReturnMiddle {
+
+    public static void main(String[] args) {
+
+        String testString = "aaabbaaa";
+        String testString2 = "aaabxaaa";
+        displayMiddleChar(testString);
+        displayMiddleChar(testString2);
+
+    }
+
+        private static void displayMiddleChar (String testString){
+
+        int length = testString.length();
+            System.out.println("String length = " + length);
+
+            if (length%2!=0){
+                int position = length /2;
+                System.out.println(testString.charAt(position));
+            }else{
+               char c2 = testString.charAt(length/2);
+               char c1 = testString.charAt(length/2-1);
+                System.out.println(String.valueOf(c1) + String.valueOf(c2));
+            }
+
+        }
+
+}
